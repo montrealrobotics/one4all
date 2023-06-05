f2b21b025cc871826d51b266fd8e087833699209
sachaMorin <morin.sacha@gmail.com>
2023-06-05 17:32:21 -0400

    Harmonize maze API with habitat API

--- run_maze.py
+++ run_maze.py
@@ -2,19 +2,19 @@ import hydra
 from omegaconf import DictConfig
 
 
-@hydra.main(version_base=None, config_path="conf/", config_name="test_maze.yaml")
+@hydra.main(version_base=None, config_path="conf/", config_name="run_maze.yaml")
 def main(cfg: DictConfig):
 
     # Imports can be nested inside @hydra.main to optimize tab completion
     # https://github.com/facebookresearch/hydra/issues/934
-    from src.test_maze import test_maze
+    from src.run_maze import run_maze
     from src import utils
 
     # Applies optional utilities
     utils.extras(cfg)
 
     # Train model
-    return test_maze(cfg)
+    return run_maze(cfg)
 
 
 if __name__ == "__main__":
